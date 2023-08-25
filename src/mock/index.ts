180b9d56b60ecd872f84ff632d5a504d13d5ad85
Artem Tyshchuk <artemtyschukk@gmail.com>
2023-08-25 19:12:52 +0200

    challenge done

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "artemtyshchuk",
+  avatar: "https://avatars.githubusercontent.com/u/103278236?v=4",
+  name: "Artem Tyshchuk",
+  company: null,
+  blog: "",
+  location: "Warsaw",
+  bio: "React developer",
+  twitter: null,
+  repos: 4,
+  followers: 0,
+  following: 1,
+  created: "2022-04-08T17:13:51Z",
+};
